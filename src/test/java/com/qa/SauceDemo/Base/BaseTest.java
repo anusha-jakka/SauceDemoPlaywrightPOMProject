2d3585ea43a1ce787ccb6770dfa7547a27572255
Anusha Jakka <anushajakka@Anushas-MacBook-Air.local>
2026-07-22 00:36:59 -0400

    Save local changes

--- src/test/java/com/qa/SauceDemo/Base/BaseTest.java
+++ src/test/java/com/qa/SauceDemo/Base/BaseTest.java
@@ -4,7 +4,9 @@ import java.util.Properties;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
+import com.beust.jcommander.Parameter;
 import com.microsoft.playwright.Page;
 import com.qa.SauceDemo.Factory.PlaywrightFactory;
 import com.qa.SauceDemo.Pages.InventoryPage;
@@ -19,12 +21,19 @@ public class BaseTest {
 	
 	protected Properties prop;
 
+	@Parameters({ "browser" })
 	@BeforeTest
-	public void setUp()
+	public void setUp(String browserName)
 	{
 		
 		playwrightLaunch = new PlaywrightFactory();
 		prop = playwrightLaunch.initProp();
+		
+		if(!browserName.isEmpty())
+		{
+			prop.setProperty("browser", browserName);
+		}
+		
 		page = playwrightLaunch.initPlaywrightBrowser(prop);
 		
 		loginPage = new LoginPage(page);
